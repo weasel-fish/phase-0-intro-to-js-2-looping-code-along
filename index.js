3fe4e4de06d7c3e67d970349d3dad4bc0107d0a8
Kyle Ermentrout <kyle.ermentrout@gmail.com>
2021-04-20 14:08:22 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // Code your solutions in this file
 
+function writeCards(names, event) {
+    const messages = [];
+
+    for(let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return messages;
+}
+
+
 function countDown(num) {
 
     while(num >= 0) {
@@ -7,5 +18,3 @@ function countDown(num) {
         num--;
     }
 }
-
-//countDown(11);
